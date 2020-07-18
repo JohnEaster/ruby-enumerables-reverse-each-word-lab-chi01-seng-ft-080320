6d99a084e1d05ea1bde83e3b14170a0d5317b4f4
John Easter <john@owltestprep.com>
2020-07-18 16:15:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence = "Hello there, how are you?")
   array = sentence.split(" ")
-  new_array = array.colect {|word| word.reverse}
+  new_array = array.collect {|word| word.reverse}
   new_array.join(" ")
 end
